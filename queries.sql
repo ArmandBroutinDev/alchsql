b598709fe2762c6df6a7f862cb778ea214eca5ad
Armand B <71330618+ArmandBroutinDev@users.noreply.github.com>
2021-02-03 12:30:36 +0100

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 select * from offices order by country,state,city;
 select count(employeeNumber) from employees;
 select sum(amount) from payments;
+select * from products where productLine LIKE('%car%');
+select sum(amount) from payments where paymentDate Like '2004-08-28';
--- sqlalch.py
+++ sqlalch.py
@@ -1,6 +1,7 @@
 from sqlalchemy import *
 import subprocess
 import sqlalchemy.dialects.mysql
+import pymysql
 
 config = {
     'host' : 'localhost',
@@ -25,12 +26,22 @@ print('--- docker ps info ---\n')
 returned_value = subprocess.call("docker ps", shell=True)
 
 print('\n--- Tables into database---\n')
-
 with engine.connect() as connection:
-    result = connection.execute("show tables")
-    print(result)
+    result = connection.execute("show tables;")
+    for row in result:
+        print(row)
 
-
+    print("\nrequete 1")
     rs = connection.execute("select * from offices order by country,state,city;")
     for row in rs:
         print(row)
+
+    print("\nrequete 2")
+    rs = connection.execute("select sum(amount) from payments;")
+    for row in rs:
+        print(row['sum(amount)'])
+
+    print("\nrequete 3")
+    rs = connection.execute("select count(employeeNumber) from employees;")
+    for row in rs:
+        print(row['count(employeeNumber)'])
